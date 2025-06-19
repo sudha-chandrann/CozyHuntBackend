559cf37197e055aff997239e0ebd73863c81e55b
sudhachandrann <sudhachandran1020@gmail.com>
2025-06-19 14:56:41 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser";
+import dotenv from 'dotenv';
+dotenv.config();
+const app = express()
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost:5173', 
+  credentials: true,
+}));
+
+app.use(express.json({
+    limit:"16kb"
+}))
+
+app.use(express.urlencoded({
+    extended:true,
+    limit:"16kb"
+}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+
+export {app}
